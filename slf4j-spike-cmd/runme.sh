4b6b12815d7305cfb8c8591ca78eaac904285ca3
jhe <le.jhe@free.fr>
2024-03-27 23:16:20 +0100

    showing that adding slf4j implementation in classpath at runtime won't work with fatjar

--- slf4j-spike-cmd/runme.sh
+++ slf4j-spike-cmd/runme.sh
@@ -1,1 +1,4 @@
-java -jar target/slf4j-spike-cmd-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+#java -jar target/slf4j-spike-cmd-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+# even when adding slf4j-simple to the classpath it won't work
+# that is because the package org.slf4j is now splitted in defferent jars : fatjar + slf4j-simple
+java -classpath /devel/r2/org/slf4j/slf4j-simple/2.0.7/slf4j-simple-2.0.7.jar -jar target/slf4j-spike-cmd-0.0.1-SNAPSHOT-jar-with-dependencies.jar
